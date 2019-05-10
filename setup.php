03bd2b5f33a44134f5ab6daab20ebc3db52f36ca
Anton Everaert <antoneveraert@gmail.com>
2019-05-10 11:55:09 +0200

    setup first form

---
+++ setup.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Setup</title>
+	<link rel="stylesheet" href=".../bootstrap.css">
+</head>
+<body>
+	<form action="result.php" method="post">
+		<h2>5 Favorite movies</h2>
+		<ul>
+			<li type="text" name="favMovie1">1</li>
+			<li type="text" name="favMovie2">2</li>
+			<li type="text" name="favMovie3">3</li>
+			<li type="text" name="favMovie4">4</li>
+			<li type="text" name="favMovie5">5</li>
+		</ul>
+		<h2>5 Favorite tv shows</h2>
+		<ul>
+			<li type="text" name="favTvShow1">1</li>
+			<li type="text" name="favTvShow2">2</li>
+			<li type="text" name="favTvShow3">3</li>
+			<li type="text" name="favTvShow4">4</li>
+			<li type="text" name="favTvShow5">5</li>
+		</ul>
+		<input type="submit">
+	</form>
+</body>
+</html>
