1c1db49b0a80bf4ffa8899c4da5c5ce19bbacbe7
Anton Everaert <antoneveraert@gmail.com>
2019-05-22 11:43:19 +0200

    added files for ex5

---
+++ Ex5/home.php
@@ -0,0 +1,13 @@
+<?php 
+session_start();
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+</head>
+<body>
+	
+</body>
+</html>
---
+++ Ex5/security.php
@@ -0,0 +1,13 @@
+<?php 
+session_start();
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Security</title>
+</head>
+<body>
+	
+</body>
+</html>
